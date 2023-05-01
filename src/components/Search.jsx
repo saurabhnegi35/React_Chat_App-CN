b953f21ed172a8b6a462ae5db6461e77d36867fa
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-01 09:57:11 +0530

    Added Search Component

---
+++ src/components/Search.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="find a user" value="" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Saurabh</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
