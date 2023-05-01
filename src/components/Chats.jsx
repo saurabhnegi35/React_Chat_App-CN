c241ae0da98b81272e50a94ba053eb47b6081374
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-01 10:05:49 +0530

    Added Chats Component

---
+++ src/components/Chats.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Saurabh</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Saurabh</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Saurabh</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Saurabh</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
