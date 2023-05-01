bf52616a29d855db0eac7951c53f447557b00ce2
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-01 10:21:26 +0530

    Added topbar in Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Add from "../images/add-user.png";
+import Camera from "../images/camera.png";
+import ThreeDot from "../images/threedot.png";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Saurabh</span>
+        <div className="chatIcons">
+          <img src={Camera} alt="" />
+          <img src={Add} alt="" />
+          <img src={ThreeDot} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
