bfe3b3833e049325a423a2ec694506be09d168c1
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-03 10:50:40 +0530

    Added Functionality to fetch private chat messages

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Add from "../images/add-user.png";
 import Camera from "../images/camera.png";
 import ThreeDot from "../images/threedot.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
+    console.log("data...............",data);
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Saurabh</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={Camera} alt="" />
           <img src={Add} alt="" />
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,10 +3,12 @@ import React, { useContext, useEffect, useState } from "react";
 import { db } from "../firebase";
 // import { createUserWithEmailAndPassword } from "firebase/auth";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -22,12 +24,21 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  console.log(Object.entries(chats));
+  //   console.log(Object.entries(chats));
+
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+    console.log("user...........", u);
+  };
 
   return (
     <div className="chats">
       {Object.entries(chats)?.map((chat) => (
-        <div className="userChat" key={chat[0]}>
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
