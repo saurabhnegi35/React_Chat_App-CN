fe9f20617ea123452090585dee3409d55097c142
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-05 11:39:58 +0530

    Added Comments to Message.jsx and Messages.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,22 +3,31 @@ import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 
 const Message = ({ message }) => {
+  // get the current user from the AuthContext
   const { currentUser } = useContext(AuthContext);
+
+  // get the chat data from the ChatContext
   const { data } = useContext(ChatContext);
 
+  // create a reference to the div element, using the useRef hook
   const ref = useRef();
 
+  // useEffect hook that scrolls to the bottom of the div element every time a new message is received
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
 
   return (
+    // a div element that holds the message data, using the ref to create a scrolling effect
     <div
       ref={ref}
+      // a conditional class that adds the "owner" class if the message was sent by the current user
       className={`message ${message.senderId === currentUser.uid && "owner"}`}
     >
+      {/* a div element that holds the message info, including sender's photo and time */}
       <div className="messageInfo">
         <img
+          // a conditional src attribute that displays the current user's photo or the other user's photo
           src={
             message.senderId === currentUser.uid
               ? currentUser.photoURL
@@ -28,6 +37,7 @@ const Message = ({ message }) => {
         />
         <span>Just Now</span>
       </div>
+      {/* a div element that holds the message content, including the text and image if any */}
       <div className="messageContent">
         <p>{message.text}</p>
         {message.img && <img src={message.img} alt="" />}
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -5,22 +5,30 @@ import { db } from "../firebase";
 import Message from "./Message";
 
 const Messages = () => {
+  // set initial state for messages using useState hook
   const [messages, setMessages] = useState([]);
+  // access chat data from context using useContext hook
   const { data } = useContext(ChatContext);
 
+  // set up useEffect hook to subscribe to changes in messages collection
   useEffect(() => {
     try {
+      // create an onSnapshot listener for the specified document in the "chats" collection
       const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+        // check if document exists and update state with messages
         doc.exists() && setMessages(doc.data().message);
+        // return an unsubscribe function to avoid memory leaks
         return () => {
           unSub();
         };
       });
     } catch (error) {
+      // log any errors
       console.log(error);
     }
   }, [data.chatId]);
 
+  // render messages using map method
   return (
     <div className="messages">
       {messages?.map((message) => (
