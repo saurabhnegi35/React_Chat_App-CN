c1ac2744fa9b530af8b79f232b3e6031f0b056f9
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-05 11:48:32 +0530

    Added Comments to Search.jsx and Sidebar.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,29 +14,39 @@ import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
+  // State to hold the value of the input field.
   const [username, setUsername] = useState("");
+  // State to hold the user data when it is found.
   const [user, setUser] = useState(null);
+  // State to hold whether an error occurs during the search.
   const [err, setErr] = useState(false);
 
+  // Access the current user from the AuthContext.
   const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
+      // Reference to the users collection in Firestore.
       collection(db, "users"),
+      // Filter the query to look for user with matching displayName.
       where("displayName", "==", username)
     );
 
     try {
+      // Execute the query.
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
+        // Set the user data to the state when a matching user is found.
         setUser(doc.data());
       });
     } catch (err) {
+      // Set the error state to true when there is an error during the search.
       setErr(true);
     }
   };
 
   const handleKey = (e) => {
+    // When the Enter key is pressed, execute the search function.
     e.code === "Enter" && handleSearch();
   };
 
@@ -45,15 +55,16 @@ const Search = () => {
     const combinedId =
       currentUser.uid > user.uid
         ? currentUser.uid + user.uid
-        : user.uid + currentUser.uid;
+        : user.uid + currentUser.uid;       // Combine the two user IDs to create the chat ID.
     try {
+      // Check if the chat already exists.
       const res = await getDoc(doc(db, "chats", combinedId));
 
       if (!res.exists()) {
-        //create a chat in chats collection
+        // If the chat doesn't exist, create a new chat document in the "chats" collection.
         await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-        //create user chats
+        // Update the "userChats" collection for both users.
         await updateDoc(doc(db, "userChats", currentUser.uid), {
           [combinedId + ".userInfo"]: {
             uid: user.uid,
@@ -74,8 +85,8 @@ const Search = () => {
       }
     } catch (err) {}
 
-    setUser(null);
-    setUsername("");
+    setUser(null);      // Reset the state to clear the user data.
+    setUsername("");    // Reset the input field.
   };
 
   return (
@@ -89,6 +100,7 @@ const Search = () => {
           value={username}
         />
       </div>
+      {/* Render an error message if there's an error during the search. */}
       {err && <span>User not found!</span>}
       {user && (
         <div className="userChat" onClick={handleSelect}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,19 @@ import Navbar from "./Navbar";
 import Search from "./Search";
 import Chats from "./Chats";
 
+// Creating a functional component called Sidebar
 const Sidebar = () => {
   return (
     <div className="sidebar">
+      {/* Rendering the Navbar component */}
       <Navbar />
+      {/* Rendering the Search component */}
       <Search />
+      {/* Rendering the Chats component */}
       <Chats />
     </div>
   );
 };
 
+// Exporting the Sidebar component as default
 export default Sidebar;
